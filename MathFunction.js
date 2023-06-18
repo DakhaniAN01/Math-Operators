eb3d73076a9c81fa72ff113eae07c43c08977647
DakhaniAN01 <113109582+DakhaniAN01@users.noreply.github.com>
2023-06-18 18:59:38 +0530

    Update MathFunction.js

--- MathFunction.js
+++ MathFunction.js
@@ -1,7 +1,7 @@
 let x = 3.14;
 
-//x = Math.round(x);
-//x = Math.floor(x);
-x = Math.ceil(x);
+x = Math.round(x); //Output : 3
+x = Math.floor(x); //Output : 3
+x = Math.ceil(x); //Output : 4
 
 console.log(x);
