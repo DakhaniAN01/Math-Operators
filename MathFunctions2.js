24374ede19b231e1db11c95aa6829c4a1c43a6b7
Adnan Dakhani <dakhani.an01@gmail.com>
2023-06-18 18:52:41 +0530

    commited

---
+++ MathFunctions2.js
@@ -0,0 +1,15 @@
+let x = 3;
+let y = 7;
+let z = 5;
+let a = 4;
+let maximum;
+let minimum;
+
+x = Math.pow(x, 2);
+a = Math.sqrt(a);
+maximum = Math.max(x, y, z);
+minimum = Math.min(x, y, z);
+console.log(x);
+console.log(a);
+console.log(maximum);
+console.log(minimum);
